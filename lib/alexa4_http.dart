413e14999829b973fa4d7a8b9b5f266292c5173d
= <alex.a4.25@yandex.ru>
2021-01-27 14:20:52 +0300

    Init chopper plugin

---
+++ lib/alexa4_http.dart
@@ -0,0 +1,9 @@
+library alexa4_http;
+
+export 'src/annotations.dart';
+export 'src/base.dart';
+export 'src/interceptor.dart';
+export 'src/constants.dart';
+export 'src/request.dart';
+export 'src/response.dart';
+export 'src/utils.dart';
